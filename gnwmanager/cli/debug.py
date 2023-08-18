1be3d1c51d0e973ffab63d21aae5689600433f9a
Brian Pugh <bnp117@gmail.com>
2023-08-18 14:37:30 -0700

    Add debug pdb command

--- gnwmanager/cli/debug.py
+++ gnwmanager/cli/debug.py
@@ -5,6 +5,7 @@ from typer import Option
 from typing_extensions import Annotated
 
 from gnwmanager.cli._parsers import int_parser
+from gnwmanager.filesystem import get_filesystem
 from gnwmanager.utils import convert_framebuffer
 
 app = typer.Typer(
@@ -38,3 +39,23 @@ def screenshot(
     img = convert_framebuffer(framebuffer)
 
     img.save(dst)
+
+
+@app.command()
+def pdb(
+    offset: Annotated[
+        int,
+        Option(
+            min=0,
+            parser=int_parser,
+            help="Distance in bytes from the END of the filesystem, to the END of flash.",
+        ),
+    ] = 0,
+):
+    """Drop into debugging with app launched."""
+    from .main import session
+
+    target = session.target
+    get_filesystem(target, offset=offset)
+
+    breakpoint()
