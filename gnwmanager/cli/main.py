6f68a412415c8d91b3c6b06c503a7c9af639359e
Brian Pugh <bnp117@gmail.com>
2023-08-18 16:20:16 -0700

    mkdir command

--- gnwmanager/cli/main.py
+++ gnwmanager/cli/main.py
@@ -8,7 +8,7 @@ from typer import Option
 import gnwmanager
 from gnwmanager.target import GnWTargetMixin, mixin_object
 
-from . import debug, disable_debug, erase, flash, ls, pull, push, screenshot, shell, start
+from . import debug, disable_debug, erase, flash, ls, mkdir, pull, push, screenshot, shell, start
 from ._start_gnwmanager import start_gnwmanager
 
 session: Session
@@ -24,6 +24,7 @@ app.command()(screenshot.screenshot)
 app.command()(pull.pull)
 app.command()(push.push)
 app.command()(disable_debug.disable_debug)
+app.command()(mkdir.mkdir)
 
 
 def version_callback(value: bool):
---
+++ gnwmanager/cli/mkdir.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timezone
+from pathlib import Path
+
+from littlefs import LittleFS, LittleFSError
+from typer import Argument, Option
+from typing_extensions import Annotated
+
+from gnwmanager.cli._parsers import int_parser
+from gnwmanager.filesystem import get_filesystem
+
+
+def mkdir(
+    path: Annotated[
+        Path,
+        Argument(
+            help="Directory to create.",
+        ),
+    ] = Path(),
+    offset: Annotated[
+        int,
+        Option(
+            min=0,
+            parser=int_parser,
+            help="Distance in bytes from the END of the filesystem, to the END of flash.",
+        ),
+    ] = 0,
+):
+    """Create a directory on device."""
+    from .main import session
+
+    target = session.target
+    fs = get_filesystem(target, offset=offset)
+
+    fs.makedirs(path.as_posix(), exist_ok=True)
