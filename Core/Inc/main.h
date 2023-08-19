0eb9814904622f5b78c9c18dcb015b764aec0a02
Brian Pugh <bnp117@gmail.com>
2023-08-19 11:23:03 -0700

    ioc update headers wip

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -52,10 +52,9 @@ extern RTC_HandleTypeDef hrtc;
 
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
-void wdog_refresh(void);
 
 /* USER CODE BEGIN EFP */
-
+void wdog_refresh(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
@@ -77,17 +76,14 @@ void wdog_refresh(void);
 #define BTN_Down_GPIO_Port GPIOD
 #define BTN_Right_Pin GPIO_PIN_15
 #define BTN_Right_GPIO_Port GPIOD
-#define BTN_Up_Pin GPIO_PIN_0
-#define BTN_Up_GPIO_Port GPIOD
-#define BTN_B_Pin GPIO_PIN_5
-#define BTN_B_GPIO_Port GPIOD
-
-// Zelda only buttons; they are not connected on mario.
 #define BTN_START_Pin GPIO_PIN_11
 #define BTN_START_GPIO_Port GPIOC
 #define BTN_SELECT_Pin GPIO_PIN_12
 #define BTN_SELECT_GPIO_Port GPIOC
-
+#define BTN_Up_Pin GPIO_PIN_0
+#define BTN_Up_GPIO_Port GPIOD
+#define BTN_B_Pin GPIO_PIN_5
+#define BTN_B_GPIO_Port GPIOD
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
