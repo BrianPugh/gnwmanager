740c46706481f19ed88d1ba8a93c773e390cb172
Brian Pugh <bnp117@gmail.com>
2023-08-20 13:45:59 -0700

    re-enable RAM

--- Core/Src/system_stm32h7xx.c
+++ Core/Src/system_stm32h7xx.c
@@ -82,9 +82,8 @@
 
 /*!< Uncomment the following line if you need to relocate your vector Table in
      Internal SRAM. */
-//#define VECT_TAB_SRAM
-#define VECT_TAB_OFFSET  0x0UL /*!< Vector Table base offset field.
-//#define VECT_TAB_OFFSET  0xE9800UL /*!< Vector Table base offset field.
+#define VECT_TAB_SRAM
+#define VECT_TAB_OFFSET  0xE6800UL /*!< Vector Table base offset field.
 //                                      This value must be a multiple of 0x200. */
 /******************************************************************************/
 
