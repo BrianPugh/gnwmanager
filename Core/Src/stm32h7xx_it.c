1dead447630e17be40dff2dc292b6380265b7081
marian <marian.muller@serli.com>
2023-09-06 22:53:21 +0200

    use error handler in fault interrupts

--- Core/Src/stm32h7xx_it.c
+++ Core/Src/stm32h7xx_it.c
@@ -69,6 +69,7 @@
 void NMI_Handler(void)
 {
   /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+  Error_Handler();
 
   /* USER CODE END NonMaskableInt_IRQn 0 */
   /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
@@ -82,6 +83,7 @@ void NMI_Handler(void)
 void HardFault_Handler(void)
 {
   /* USER CODE BEGIN HardFault_IRQn 0 */
+  Error_Handler();
 
   /* USER CODE END HardFault_IRQn 0 */
   while (1)
@@ -97,6 +99,7 @@ void HardFault_Handler(void)
 void MemManage_Handler(void)
 {
   /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+  Error_Handler();
 
   /* USER CODE END MemoryManagement_IRQn 0 */
   while (1)
@@ -112,6 +115,7 @@ void MemManage_Handler(void)
 void BusFault_Handler(void)
 {
   /* USER CODE BEGIN BusFault_IRQn 0 */
+  Error_Handler();
 
   /* USER CODE END BusFault_IRQn 0 */
   while (1)
@@ -127,6 +131,7 @@ void BusFault_Handler(void)
 void UsageFault_Handler(void)
 {
   /* USER CODE BEGIN UsageFault_IRQn 0 */
+  Error_Handler();
 
   /* USER CODE END UsageFault_IRQn 0 */
   while (1)
