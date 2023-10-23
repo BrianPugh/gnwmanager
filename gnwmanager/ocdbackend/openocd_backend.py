1de11ec0f9582a02a55fe4adac52a579973ee5db
Brian Pugh <bnp117@gmail.com>
2023-10-22 19:56:05 -0700

    openocd autodetect

--- gnwmanager/ocdbackend/openocd_backend.py
+++ gnwmanager/ocdbackend/openocd_backend.py
@@ -2,6 +2,7 @@ import os
 import socket
 import subprocess
 from time import sleep
+from typing import Generator, List
 
 from gnwmanager.ocdbackend.base import OCDBackend
 
@@ -10,24 +11,73 @@ _COMMAND_TOKEN_BYTES = _COMMAND_TOKEN_STR.encode("utf-8")
 _BUFFER_SIZE = 4096
 
 
+class OpenOCDAutoDetectError(Exception):
+    """Was unable to detect a debugging probe."""
+
+
+def _openocd_launch_commands(port: int) -> Generator[List[str], None, None]:
+    openocd_executable = os.environ.get("OPENOCD", "openocd")
+    base_cmd = [
+        openocd_executable,
+        "-c",
+        f"tcl_port {port}",
+    ]
+
+    # STLink
+    cmd = base_cmd.copy()
+    cmd.extend(["-c", "source [find interface/stlink.cfg]"])
+    cmd.extend(["-c", "adapter speed 500"])
+    cmd.extend(["-c", "transport select hla_swd"])
+    cmd.extend(["-c", "source [find target/stm32h7x.cfg]"])
+    yield cmd
+
+    # Raspberry Pi GPIO
+    cmd = base_cmd.copy()
+    cmd.extend(["-c", "source [find interface/sysfsgpio-raspberrypi.cfg]"])
+    cmd.extend(["-c", "source [find openocd/rpi.cfg]"])
+    cmd.extend(["-c", "transport select swd"])
+    cmd.extend(["-c", "source [find target/stm32h7x.cfg]"])
+    yield cmd
+
+    # J-Link
+    cmd = base_cmd.copy()
+    cmd.extend(["-c", "source [find interface/jlink.cfg]"])
+    cmd.extend(["-c", "adapter speed 500"])
+    cmd.extend(["-c", "transport select swd"])
+    cmd.extend(["-c", "source [find target/stm32h7x.cfg]"])
+    yield cmd
+
+    # CMSIS-DAP
+    cmd = base_cmd.copy()
+    cmd.extend(["-c", "source [find interface/cmsis-dap.cfg]"])
+    cmd.extend(["-c", "adapter speed 500"])
+    cmd.extend(["-c", "transport select swd"])
+    cmd.extend(["-c", "source [find target/stm32h7x.cfg]"])
+    yield cmd
+
+
+def _launch_openocd(port: int) -> subprocess.Popen[bytes]:
+    for cmd in _openocd_launch_commands(port):
+        print(cmd)
+        # TODO: swallow IO
+        process = subprocess.Popen(cmd)
+        sleep(0.3)
+        if process.poll() is None:
+            # Process is still running
+            # it didn't immediately close due to not detecting probe.
+            return process
+    raise OpenOCDAutoDetectError
+
+
 class OpenOCDBackend(OCDBackend):
     def __init__(self, connect_mode="attach", port=6666):
         super().__init__()
         self._address = ("localhost", port)
-
-        openocd_executable = os.environ.get("OPENOCD", "openocd")
-        self._openocd_cmd = [
-            openocd_executable,
-            "-c",
-            f"tcl_port {port}",
-        ]
-
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._openocd_process = None
 
     def open(self) -> OCDBackend:
-        self._openocd_process = subprocess.Popen(self._openocd_cmd)
-        sleep(5.0)
+        self._openocd_process = _launch_openocd(self._address[1])
         self._socket.connect(self._address)
         return self
 
@@ -61,7 +111,7 @@ class OpenOCDBackend(OCDBackend):
 
     def read_memory(self, addr: int, size: int) -> bytes:
         """Reads a block of memory."""
-        return self("read_memory 0x{addr:08X} {size} 1")
+        return self(f"read_memory 0x{addr:08X} 8 {size}")
 
     def write_memory(self, addr: int, data: bytes):
         """Writes a block of memory."""
@@ -78,7 +128,8 @@ class OpenOCDBackend(OCDBackend):
 
     def set_frequency(self, freq: int):
         """Set probe frequency in hertz."""
-        raise NotImplementedError
+        freq_khz = round(freq / 1000)
+        self(f"adapter speed {freq_khz}")
 
     def reset(self):
         """Reset target."""
