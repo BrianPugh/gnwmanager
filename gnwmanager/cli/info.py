3602c9fe49759b2da232849c3b2d02e97b49b66a
Brian Pugh <bnp117@gmail.com>
2023-11-01 18:56:17 -0700

    fix info command when device autodetection fails

--- gnwmanager/cli/info.py
+++ gnwmanager/cli/info.py
@@ -1,4 +1,4 @@
-from .unlock import DeviceModel
+from .unlock import AutodetectError, DeviceModel
 
 
 def display(field, value):
@@ -14,7 +14,11 @@ def info():
 
     display("Debug Probe:", gnw.backend.probe_name)
 
-    device = DeviceModel.autodetect(gnw)
+    try:
+        device = DeviceModel.autodetect(gnw)
+    except AutodetectError:
+        device = "unknown"
+
     display("Detected Device:", str(device).upper())
 
     display("External Flash Size (MB):", str(gnw.external_flash_size / (1 << 20)))
