f94836baa4d5a3142deab6fc44ae82c6d6bb0841
Brian Pugh <bnp117@gmail.com>
2023-12-01 19:09:42 -0800

    cyclopts format

--- gnwmanager/cli/__init__.py
+++ gnwmanager/cli/__init__.py
@@ -1,2 +1,3 @@
+from gnwmanager.cli._format import format
 from gnwmanager.cli._info import info
 from gnwmanager.cli._ls import ls
--- gnwmanager/cli/_format.py
+++ gnwmanager/cli/_format.py
@@ -1,8 +1,7 @@
 from littlefs import LittleFSError
-from typer import Option
-from typing_extensions import Annotated
 
-from gnwmanager.cli._parsers import int_parser
+from gnwmanager.cli._parsers import GnWType, OffsetType
+from gnwmanager.cli.main import app
 
 
 def _infer_block_count(gnw, offset):
@@ -15,28 +14,23 @@ def _infer_block_count(gnw, offset):
     return fs.fs_stat().block_count
 
 
+@app.command
 def format(
-    size: Annotated[
-        int,
-        Option(
-            min=0,
-            parser=int_parser,
-            show_default=False,
-            help="Size of filesystem. Defaults to previous filesystem size.",
-        ),
-    ] = 0,
-    offset: Annotated[
-        int,
-        Option(
-            min=0,
-            parser=int_parser,
-            help="Distance in bytes from the END of the filesystem, to the END of flash.",
-        ),
-    ] = 0,
+    size: OffsetType = 0,
+    offset: OffsetType = 0,
+    *,
+    gnw: GnWType,
 ):
-    """Format device's filesystem."""
-    from .main import gnw
+    """Format device's filesystem.
 
+    Parameters
+    ----------
+    size
+        Size of filesystem. Defaults to previous filesystem size.
+    offset
+        Distance in bytes from the END of the filesystem, to the END of flash.
+    """
+    gnw.start_gnwmanager()
     if size > gnw.external_flash_size:
         raise ValueError(f"--size must be <= detected flash size {gnw.external_flash_size}.")
     if size % gnw.external_flash_block_size != 0:
